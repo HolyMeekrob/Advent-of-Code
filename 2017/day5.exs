9c5ba3dc76acd4ab63e21da999e1a91dd022905f
Holy Meekrob <holy.meekrob@gmail.com>
2017-12-05 07:59:21 -0600

    2017 day five

---
+++ 2017/day5.exs
@@ -0,0 +1,66 @@
+defmodule DayFive do
+	def part_one(instructions) do
+		jump_one(instructions, 0, 0)
+	end
+
+	def part_two(instructions) do
+		jump_two(instructions, 0, 0)
+	end
+
+	defp jump_one(_, index, count) when index < 0 do
+		count
+	end
+
+	defp jump_one(instructions, index, count) do
+		case Enum.fetch(instructions, index) do
+			:error -> count
+			{:ok, instruction} ->
+				new_index = index + instruction
+				new_instructions = List.replace_at(instructions, index, instruction + 1)
+				jump_one(new_instructions, new_index, count + 1)
+		end
+	end
+
+	defp jump_two(_, index, count) when index < 0 do
+		count
+	end
+
+	defp jump_two(instructions, index, count) do
+		case Enum.fetch(instructions, index) do
+			:error -> count
+			{:ok, instruction} ->
+				new_index = index + instruction
+				new_instructions = List.replace_at(instructions, index,
+					get_new_instruction(instruction))
+				jump_two(new_instructions, new_index, count + 1)
+		end
+	end
+
+	defp get_new_instruction(instruction) do
+		if (instruction > 2) do
+			instruction - 1
+		else
+			instruction + 1
+		end
+	end
+end
+
+input =
+	if (length(System.argv) == 0) do
+		"input/day5.txt"
+			|> File.read!
+			|> String.split("\r\n")
+			|> Enum.map(&String.to_integer/1)
+	else
+		System.argv
+		|> List.first
+		|> String.split
+		|> Enum.map(&String.to_integer/1)
+	end
+
+# Expected answers for default input
+# Part one: 336905
+# Part two: 21985262
+
+IO.puts("Part one: " <> Integer.to_string(DayFive.part_one(input)))
+IO.puts("Part two: " <> Integer.to_string(DayFive.part_two(input)))
