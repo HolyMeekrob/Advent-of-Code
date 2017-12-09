c38a0a74a7af448c478990a24f95f40f34cfe50b
Holy Meekrob <holy.meekrob@gmail.com>
2017-12-09 08:51:30 -0600

    2017 day nine part one

---
+++ 2017/day9.exs
@@ -0,0 +1,64 @@
+defmodule StreamState do
+	defstruct score: 0, depth: 0, in_comment: false, ignore_next: false
+end
+
+defmodule DayNine do
+	def part_one(stream) do
+		final_state = read_next(stream, %StreamState{})
+		final_state.score
+	end
+
+	defp read_next(stream, state) do
+		case (IO.read(stream, 1)) do
+			:eof -> state
+			x -> read_next(stream, process(x, state))
+		end 
+	end
+
+	defp process(_, %{in_comment: true, ignore_next: true} = state) do
+		%{state | ignore_next: false}
+	end
+
+	defp process("!", %{in_comment: true} = state) do
+		%{state | ignore_next: true}
+	end
+
+	defp process(">", %{in_comment: true} = state) do
+		%{state | in_comment: false}
+	end
+
+	defp process(_, %{in_comment: true} = state) do
+		state
+	end
+
+	defp process("<", state) do
+		%{state | in_comment: true}
+	end
+
+	defp process("{", state) do
+		%{state | depth: state.depth + 1}
+	end
+
+	defp process("}", state) do
+		%{state | score: state.score + state.depth, depth: state.depth - 1}
+	end
+
+	defp process(_, state) do
+		state
+	end
+end
+
+input =
+	if (length(System.argv) == 0) do
+		File.open!("input/day9.txt", [:read, :utf8])
+	else
+		System.argv
+		|> List.first
+	end
+
+# Expected answers for default input
+# Part one: 17390
+# Part two: 
+
+IO.puts("Part one: " <> Integer.to_string(DayNine.part_one(input)))
+#IO.puts("Part two: " <> Integer.to_string(DayNine.part_two(input)))
