183a841b04b573fd9da53abba5d619dc69d04eb1
Holy Meekrob <holy.meekrob@gmail.com>
2023-12-02 23:15:30 -0600

    2023 day 2 part 2

--- 2023/Days/Day2.cs
+++ 2023/Days/Day2.cs
@@ -7,26 +7,73 @@ namespace _2023.Days;
 
 public sealed partial class Day2 : IDay
 {
-	public (string PartOne, string PartTwo) Run(bool isTest) =>
-		(RunPuzzleOne(isTest).ToString(), RunPuzzleTwo(isTest));
+	public (string PartOne, string PartTwo) Run(bool isTest)
+	{
+		var games = GetAllLines(isTest, 2).Select(CreateGame).ToList();
+		return (RunPuzzleOne(games).ToString(), RunPuzzleTwo(games).ToString());
+	}
 
 	#region Puzzle one
 
-	private static int RunPuzzleOne(bool isTest)
+	private static int RunPuzzleOne(IReadOnlyCollection<Game> games)
 	{
-		var lines = GetAllLines(isTest, 2);
-		var games = GetAllLines(isTest, 2).Select(CreateGame));
-
 		var contents = new Reveal(
 			new Dictionary<Color, int>
 			{
 				{ Color.Red, 12 },
 				{ Color.Green, 13 },
 				{ Color.Blue, 14 },
-			});
+			}
+		);
 
 		return games.Where(game => IsPossible(contents, game)).Select(game => game.Id).Sum();
 	}
+
+	private static bool IsPossible(Reveal contents, Game game) =>
+		game.Reveals.All(reveal => IsPossible(contents, reveal));
+
+	private static bool IsPossible(Reveal contents, Reveal reveal)
+	{
+		contents.TryGetValue(Color.Red, out var redContents);
+		reveal.TryGetValue(Color.Red, out var redReveal);
+		contents.TryGetValue(Color.Green, out var greenContents);
+		reveal.TryGetValue(Color.Green, out var greenReveal);
+		contents.TryGetValue(Color.Blue, out var blueContents);
+		reveal.TryGetValue(Color.Blue, out var blueReveal);
+
+		return redContents >= redReveal
+			&& greenContents >= greenReveal
+			&& blueContents >= blueReveal;
+	}
+
+	#endregion Puzzle one
+
+	#region Puzzle two
+
+	private static int RunPuzzleTwo(IReadOnlyCollection<Game> games) =>
+		games.Select(GetPower).Sum();
+
+	private static int GetPower(Game game) =>
+		GetRequiredCount(Color.Red, game.Reveals)
+		* GetRequiredCount(Color.Green, game.Reveals)
+		* GetRequiredCount(Color.Blue, game.Reveals);
+
+	private static int GetRequiredCount(Color color, IReadOnlyCollection<Reveal> reveals) =>
+		reveals
+			.Select(reveal =>
+			{
+				reveal.TryGetValue(color, out var count);
+				return count;
+			})
+			.Max();
+
+	#endregion Puzzle two
+
+	[GeneratedRegex(@"^Game (?<Id>\d+): (?<Reveals>.*)$", RegexOptions.Compiled)]
+	private static partial Regex GameRegex();
+
+	[GeneratedRegex(@"(?<Count>\d+) (?<Color>(?:red|green|blue))")]
+	private static partial Regex RevealColorRegex();
 
 	private static Game CreateGame(string line)
 	{
@@ -41,7 +88,8 @@ public sealed partial class Day2 : IDay
 			text.Trim()
 				.Split(",")
 				.Select(CreateRevealColor)
-				.ToDictionary(revealColor => revealColor.Color, revealColor => revealColor.Count));
+				.ToDictionary(revealColor => revealColor.Color, revealColor => revealColor.Count)
+		);
 
 	private static RevealColor CreateRevealColor(string text)
 	{
@@ -51,33 +99,7 @@ public sealed partial class Day2 : IDay
 		return new RevealColor(count, color);
 	}
 
-	private static bool IsPossible(Reveal contents, Game game) =>
-		game.Reveals.All(reveal => IsPossible(contents, reveal));
-	
-	private static bool IsPossible(Reveal contents, Reveal reveal)
-	{
-		contents.TryGetValue(Color.Red, out var redContents);
-		reveal.TryGetValue(Color.Red, out var redReveal);
-		contents.TryGetValue(Color.Green, out var greenContents);
-		reveal.TryGetValue(Color.Green, out var greenReveal);
-		contents.TryGetValue(Color.Blue, out var blueContents);
-		reveal.TryGetValue(Color.Blue, out var blueReveal);
+	private sealed record Game(int Id, IReadOnlyCollection<Reveal> Reveals);
 
-		return redContents >= redReveal && greenContents >= greenReveal && blueContents >= blueReveal;
-	}
-
-	#endregion Puzzle one
-	
-	#region Puzzle two
-	
-	private static string RunPuzzleTwo(bool isTest) => "Not implemented";
-    [GeneratedRegex(@"^Game (?<Id>\d+): (?<Reveals>.*)$", RegexOptions.Compiled)]
-    private static partial Regex GameRegex();
-    [GeneratedRegex(@"(?<Count>\d+) (?<Color>(?:red|green|blue))")]
-    private static partial Regex RevealColorRegex();
-
-    #endregion Puzzle two
-
-    private sealed record Game(int Id, IReadOnlyCollection<Reveal> Reveals);
-    private sealed record RevealColor(int Count, Color Color);
+	private sealed record RevealColor(int Count, Color Color);
 }
