90ce987ad48221fa175d688e9bff7b15ae8afc5f
Holy Meekrob <holy.meekrob@gmail.com>
2023-12-12 21:21:41 -0600

    2023 day 9 part 2

--- 2023/Days/Day9.cs
+++ 2023/Days/Day9.cs
@@ -7,43 +7,23 @@ public class Day9 : IDay
 {
 	public (string PartOne, string PartTwo) Run(bool isTest)
 	{
-		var lines = GetAllLines(isTest, 9);
-		return (RunPuzzleOne(lines).ToString(), RunPuzzleTwo(lines).ToString());
+		var history = GetAllLines(isTest, 9).Select(GetHistory).ToList();
+		return (RunPuzzleOne(history).ToString(), RunPuzzleTwo(history).ToString());
 	}
 
 	#region Puzzle one
 
-	private static int RunPuzzleOne(IReadOnlyList<string> lines) =>
-		lines.Select(GetHistory).Select(GetNextStep).Sum();
-
-	private static List<int> GetHistory(string line) =>
-		line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+	private static int RunPuzzleOne(IReadOnlyList<IReadOnlyList<int>> histories) =>
+		histories.Select(GetNextStep).Sum();
 
 	private static int GetNextStep(IReadOnlyList<int> history)
 	{
 		var sequences = BuildSequences([history]);
-		sequences = AddPrediction(sequences);
+		sequences = AddNextPrediction(sequences);
 		return sequences.Last().Last();
 	}
 
-	private static Sequence BuildSequences(Sequence sequences)
-	{
-		var lastSequence = sequences.Last();
-		if (lastSequence.All(x => x == 0))
-		{
-			return sequences;
-		}
-
-		var nextSequence = new List<int>();
-		for (var i = 0; i < lastSequence.Count - 1; ++i)
-		{
-			nextSequence.Add(lastSequence[i + 1] - lastSequence[i]);
-		}
-
-		return BuildSequences(sequences.Append(nextSequence).ToList());
-	}
-
-	private static Sequence AddPrediction(Sequence sequences)
+	private static Sequence AddNextPrediction(Sequence sequences)
 	{
 		var predictedSequences = new List<List<int>>();
 		for (var i = sequences.Count - 1; i >= 0; --i)
@@ -67,10 +47,61 @@ public class Day9 : IDay
 
 	#region Puzzle two
 
-	private static int RunPuzzleTwo(IReadOnlyList<string> lines)
+	private static int RunPuzzleTwo(IReadOnlyList<IReadOnlyList<int>> histories) =>
+		histories.Select(GetPreviousStep).Sum();
+
+	private static int GetPreviousStep(IReadOnlyList<int> history)
 	{
-		return 0;
+		var sequences = BuildSequences([history]);
+		var updatedSequences = AddPreviousPrediction(sequences);
+		return updatedSequences.First().First();
+	}
+
+	private static LinkedList<LinkedList<int>> AddPreviousPrediction(Sequence sequences)
+	{
+		var predictedSequences = new LinkedList<LinkedList<int>>();
+		for (var i = sequences.Count - 1; i >= 0; --i)
+		{
+			var predictedSequence = new LinkedList<int>(sequences[i]);
+			if (i == sequences.Count - 1)
+			{
+				predictedSequence.AddFirst(0);
+			}
+			else
+			{
+				predictedSequence.AddFirst(
+					predictedSequence.First() - predictedSequences.First().First()
+				);
+			}
+			predictedSequences.AddFirst(predictedSequence);
+		}
+
+		return predictedSequences;
 	}
 
 	#endregion Puzzle two
+
+	#region Common
+
+	private static List<int> GetHistory(string line) =>
+		line.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+	private static Sequence BuildSequences(Sequence sequences)
+	{
+		var lastSequence = sequences.Last();
+		if (lastSequence.All(x => x == 0))
+		{
+			return sequences;
+		}
+
+		var nextSequence = new List<int>();
+		for (var i = 0; i < lastSequence.Count - 1; ++i)
+		{
+			nextSequence.Add(lastSequence[i + 1] - lastSequence[i]);
+		}
+
+		return BuildSequences(sequences.Append(nextSequence).ToList());
+	}
+
+	#endregion Common
 }
